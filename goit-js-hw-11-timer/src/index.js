280b03c86fae05d986c8e61c6a3334e2f3fb3112
MironenkoNadiia <candleflame@i.ua>
2020-11-11 23:35:22 +0200

    done

--- goit-js-hw-11-timer/src/index.js
+++ goit-js-hw-11-timer/src/index.js
@@ -1,30 +1,21 @@
 import './styles.css';
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const refs = require('./refs');
+    
+setInterval(() => {
+  let time = Date.parse('dec 31, 2020; 20:59') - Date.now();
+  let days = Math.floor(time / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  let secs = Math.floor((time % (1000 * 60)) / 1000);
+    refs.days.innerHTML = `${days}`;
+    refs.hours.innerHTML = `${hours}`;
+    refs.mins.innerHTML = `${mins}`;
+    refs.secs.innerHTML = `${secs}`;
+}, 1000);
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
---
+++ goit-js-hw-11-timer/src/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+    days: document.querySelector('span[data-value="days"]'),
+    hours: document.querySelector('span[data-value="hours"]'),
+    mins: document.querySelector('span[data-value="mins"]'),
+    secs: document.querySelector('span[data-value="secs"]'),
+    counterDisplay: document.querySelector('#timer-1')
+}
+
+module.exports = refs;
